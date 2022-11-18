540f429bef7c6b1a4d0ef13f29ade3bbe54b95ce
Lee Sang Hoon <nicet.shlee@gmail.com>
2022-11-18 23:45:15 +0900

    Update 정수 내림차순으로 배치하기.java

--- 프로그래머스/lv1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.java
+++ 프로그래머스/lv1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.java
@@ -3,19 +3,19 @@ import java.util.Arrays;
 class Solution {
     public long solution(long n) {
         long answer = 0;
-        String result = String.valueOf(n);
-        int[] arrays = new int[result.length()];
-        String answer2 = "";
+        String result = String.valueOf(n); // 매개변수 자릿수별로 뜯기위해 문자열로 변환
+        int[] arrays = new int[result.length()]; // 문자열로 변환된 result정보로 배열 생성
+        String answer2 = ""; 
         
         for (int i = 0; i < arrays.length; i++) {
-            arrays[i] = Integer.parseInt(String.valueOf(result.charAt(i)));
+            arrays[i] = Integer.parseInt(String.valueOf(result.charAt(i))); // array배열에 한글자씩 삽입
         }
-        Arrays.sort(arrays);
+        Arrays.sort(arrays); // 배열 오름차순 정렬
         
         for (int i = arrays.length - 1; i >= 0; i--) {
-            answer2 += arrays[i];
+            answer2 += arrays[i];  // 미리 생성해둔 문자열 answer2에 배열 값 하나씩 추가 (문자열더하기)
         }
-        answer = Long.parseLong(answer2);
+        answer = Long.parseLong(answer2); // 다 더해둔 문자열 long 으로 형변환 후 long 변수 answer로 
         return answer;
     }
 }
