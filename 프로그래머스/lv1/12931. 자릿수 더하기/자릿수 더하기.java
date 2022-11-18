b4257a446d967432e77e2cc0a4e6b013c503e7ca
Lee Sang Hoon <nicet.shlee@gmail.com>
2022-11-18 23:23:22 +0900

    Update 자릿수 더하기.java

--- 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.java
+++ 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.java
@@ -3,10 +3,10 @@ import java.util.*;
 public class Solution {
     public int solution(int n) {
         int answer = 0;
-        String result = String.valueOf(n);
+        String result = String.valueOf(n); // int 자료형을 문자열로 변환시킨 후
         
-        for (int i = 0; i < result.length(); i++) {
-            answer += Integer.parseInt(String.valueOf(result.charAt(i)));
+        for (int i = 0; i < result.length(); i++) { // 문자열의 길이만큼 반복
+            answer += Integer.parseInt(String.valueOf(result.charAt(i))); // int answer변수에 int형으로 변환해가며 한자릿수씩 더하기
         }
 
         return answer;
