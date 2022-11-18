4d0e734b6f2c6657d339663720c0c7b1b37e8e59
Lee Sang Hoon <nicet.shlee@gmail.com>
2022-11-18 23:48:12 +0900

    Update 정수 제곱근 판별.java

--- 프로그래머스/lv1/12934. 정수 제곱근 판별/정수 제곱근 판별.java
+++ 프로그래머스/lv1/12934. 정수 제곱근 판별/정수 제곱근 판별.java
@@ -1,11 +1,11 @@
 class Solution {
     public long solution(long n) {
         long answer = 0;
-        long result = (long)Math.sqrt(n);
-        if (Math.pow(result, 2) == n) {
-            answer = (long) Math.pow(result + 1, 2);
+        long result = (long)Math.sqrt(n); // 제곱근을 구할 수 있는 함수
+        if (Math.pow(result, 2) == n) { // result의 제곱근(2)이 n 과같은 수 일 경우
+            answer = (long) Math.pow(result + 1, 2); // answer값에 제곱근 +1 값에 제곱
         }else {
-            answer = -1;
+            answer = -1; // 경우의 수에 해당하지 않을 경우 -1
         }
         return answer;
     }
