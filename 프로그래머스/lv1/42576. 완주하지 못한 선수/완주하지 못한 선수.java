a5d5056c33f8da8d2d885bea592735e9bf24612b
Lee Sang Hoon <nicet.shlee@gmail.com>
2022-11-18 13:41:57 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 302.09 ms, Memory: 96.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+
+
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+        String answer = "";
+        Arrays.sort(participant);
+        Arrays.sort(completion);
+        
+        for (int i = 0; i < completion.length; i++) {
+            if (participant[i].equals(completion[i])) {
+
+            }else {
+                answer += participant[i];
+                break;
+            }
+        }
+        if (answer == "") {
+            answer = participant[participant.length - 1];
+        }
+        
+        return answer;
+    }
+}
