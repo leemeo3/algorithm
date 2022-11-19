f6542e79676d3a71e57cdca5be306eedefac9e5e
Lee Sang Hoon <nicet.shlee@gmail.com>
2022-11-19 10:01:08 +0900

    [level 1] Title: 2016년, Time: 0.02 ms, Memory: 74.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12901. 2016년/2016년.java
@@ -0,0 +1,28 @@
+class Solution {
+  public String solution(int a, int b) {
+      String answer = "";
+    int[] day= {31,29,31,30,31,30,31,31,30,31,30,31};
+    int sum=0;
+    for(int i=0;i<a-1;i++){
+        sum+=day[i];
+    }    
+    sum+=b;
+      switch(sum%7){
+          case 0:
+              return "THU";
+          case 1:
+              return "FRI";
+          case 2:
+              return "SAT";
+          case 3:
+              return "SUN";
+          case 4:
+              return "MON";
+          case 5:
+              return "TUE";
+          case 6:
+              return "WED";
+      }
+      return null;
+  }
+}
