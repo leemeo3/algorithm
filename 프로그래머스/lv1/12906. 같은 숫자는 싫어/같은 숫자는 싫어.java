703d178504d9e936a8d552c41e9b95ed6c3e5fb0
Lee Sang Hoon <nicet.shlee@gmail.com>
2022-11-19 16:28:39 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 25.39 ms, Memory: 112 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Solution {
+    public int[] solution(int []arr) {
+        ArrayList<Integer> list = new ArrayList<>();
+        int start = arr[0];
+        list.add(start);
+        for (int i = 1; i < arr.length; i++) {
+            if (start != arr[i]) {
+                start = arr[i];
+                list.add(arr[i]);
+            }
+        }
+        
+        int[] answer = new int[list.size()];
+        for (int i = 0; i < answer.length; i++) {
+            answer[i] = list.get(i);
+        }
+        return answer;
+    }
+}
