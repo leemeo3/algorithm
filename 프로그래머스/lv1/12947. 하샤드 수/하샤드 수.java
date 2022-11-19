c994b3ff3d9a4cb38400c9f5150fe6a53586acbf
Lee Sang Hoon <nicet.shlee@gmail.com>
2022-11-19 21:40:11 +0900

    [level 1] Title: 하샤드 수, Time: 0.03 ms, Memory: 76.6 MB -BaekjoonHub

--- 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.java
+++ 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.java
@@ -2,23 +2,22 @@ import java.util.ArrayList;
 
 class Solution {
     public boolean solution(int x) {
-        boolean answer = true;
-        int x2 = x; // 변수 복사 이유는 while문을 돌리면서 원본에 덮어씌우기때문
+        int x2 = x;
         int sum = 0;
         ArrayList<Integer> list = new ArrayList<>();
         
-        while (x2 > 0) { // 복사한 배열을 사용해 
+        while (x2 > 0) {
             list.add(x2 % 10);
             x2 /= 10;
         }
         for (int i = 0; i < list.size(); i++) {
             sum += list.get(i);
         }
-        if (x % sum == 0) { // 여기서 원본의 변수를 사용
-            answer = true;
+        if (x % sum == 0) {
+            return true;
         }else {
-            answer = false;
+            return false;
         }
-        return answer;
+        // return answer;
     }
 }
