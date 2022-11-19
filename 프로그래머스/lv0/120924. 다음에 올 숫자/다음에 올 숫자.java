a6a8df6896e76df79c797a7fb881c4b52b5e1827
Lee Sang Hoon <nicet.shlee@gmail.com>
2022-11-19 19:24:56 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.04 ms, Memory: 74.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120924. 다음에 올 숫자/다음에 올 숫자.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(int[] common) {
+        int answer = 0;
+        
+        if (common[1] - common[0] == common[2] - common[1]) {
+            int a = common[1] - common[0];
+            answer = common[common.length-1] + a;
+        }else if (common[1] / common[0] == common[2] / common[1]) {
+            int b = common[1] / common[0];
+            answer = common[common.length-1] * b;
+        }
+        
+        return answer;
+    }
+}
