36902a8bf4de4b1a0d94d93a327007f66952488e
Lee Sang Hoon <nicet.shlee@gmail.com>
2022-11-20 22:47:27 +0900

    [level 1] Title: 하샤드 수, Time: 0.02 ms, Memory: 73.2 MB -BaekjoonHub

--- 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.java
+++ 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.java
@@ -1,25 +1,17 @@
-import java.util.ArrayList;
-
 class Solution {
     public boolean solution(int x) {
-        int x2 = x; // 변수에 값을 복사한다.
-        int sum = 0; // 자릿수를 나눈 수를 모두 더할 변수 선언
-        ArrayList<Integer> list = new ArrayList<>(); // 리스트 선언
+        int x2 = x;
+        int sum = 0;
         
-        while (x2 > 0) { // 모든 자릿수를 검사할때까지 반복한다.
-            list.add(x2 % 10); // 리스트에 값을 담는다. 근데 생각해보니 이 값은 sum += (x2 % 10)으로도 처리가 가능했고 그렇다면 리스트를 만들지 않아도 디ㅗㄴ다.
-            x2 /= 10; // 계속 나눈다.
+        while (x2 > 0) {
+            sum += x2 % 10; // 내가 변수에 연산한 값을 바로 넣는다는게 어색해서 그런거 같다.
+                            // 아마도 한번에 한가지 일만 수행하는게 가독성이 더 좋다라고 판단한 오류아닐까?
+            x2 /= 10;
         }
-        for (int i = 0; i < list.size(); i++) { // 이 반복문은 결국 선언하지 않아도 됐다. 반복문안에 짧은 코드 특히 저런 한줄만 들어간다면
-                                                // 이유를 고민해보고 줄일수있다면 줄이자 아마 잘 생각하면 줄일수 있는 방법이 있을것이다.
-                                                // 가독성을 오히려 늘리는 선에서..
-            sum += list.get(i);
-        }
-        if (x % sum == 0) {     // 나누어떨어지는지 검사하고 answer에 담지않고 바로 
+        if (x % sum == 0) {
             return true;
         }else {
             return false;
         }
-        // return answer;
     }
 }
