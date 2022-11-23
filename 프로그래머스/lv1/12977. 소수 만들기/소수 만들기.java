a9319899705522c4efaa884e5b435ec4fcdb40a8
Lee Sang Hoon <nicet.shlee@gmail.com>
2022-11-23 21:00:35 +0900

    [level 1] Title: 소수 만들기, Time: 0.02 ms, Memory: 74.8 MB -BaekjoonHub

--- 프로그래머스/lv1/12977. 소수 만들기/소수 만들기.java
+++ 프로그래머스/lv1/12977. 소수 만들기/소수 만들기.java
@@ -1,19 +1,19 @@
 class Solution {
     public int solution(int[] nums) {
         int answer = 0;
-        int flag = 0;
-
+        int total = 0;
+        int cnt = 0;
         for (int i = 0; i < nums.length; i++) {
             for (int j = i + 1; j < nums.length; j++) {
                 for (int k = j + 1; k < nums.length; k++) {
-                    flag = 0;
-                    int cnt = nums[i] + nums[j] + nums[k];
-                    for (int l = 1; l < cnt + 1; l++) {
-                        if (cnt % l == 0) {
-                            flag += 1; // 나누어 떨어지는 숫자 카운트
+                    total = nums[i] + nums[j] + nums[k];
+                    cnt = 0;
+                    for (int l = 1; l < total + 1; l++) {
+                        if (total % l == 0) {
+                            cnt += 1; // 나누어 떨어지는 숫자 카운트
                         }
                     }
-                    if (flag == 2) { // 나누어떨어지는 숫자가 2개뿐일때
+                    if (cnt == 2) { // 나누어떨어지는 숫자가 2개뿐일때
                         answer += 1;
                     }
                 }
