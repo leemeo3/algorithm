5a6ef517d406d404a44235ae1385c3174b1c6927
Lee Sang Hoon <nicet.shlee@gmail.com>
2022-11-24 21:43:50 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.19 ms, Memory: 74.4 MB -BaekjoonHub

--- 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
+++ 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
@@ -2,16 +2,16 @@ class Solution {
     public int solution(String s) {
         int answer = 0;
         for (int i = 0; i < 9; i++) {
-            s = s.replaceAll("zero", String.valueOf(0));
-            s = s.replaceAll("one", String.valueOf(1));
-            s = s.replaceAll("two", String.valueOf(2));
-            s = s.replaceAll("three", String.valueOf(3));
-            s = s.replaceAll("four", String.valueOf(4));
-            s = s.replaceAll("five", String.valueOf(5));
-            s = s.replaceAll("six", String.valueOf(6));
-            s = s.replaceAll("seven", String.valueOf(7));
-            s = s.replaceAll("eight", String.valueOf(8));
-            s = s.replaceAll("nine", String.valueOf(9));
+            s = s.replace("zero", String.valueOf(0));
+            s = s.replace("one", String.valueOf(1));
+            s = s.replace("two", String.valueOf(2));
+            s = s.replace("three", String.valueOf(3));
+            s = s.replace("four", String.valueOf(4));
+            s = s.replace("five", String.valueOf(5));
+            s = s.replace("six", String.valueOf(6));
+            s = s.replace("seven", String.valueOf(7));
+            s = s.replace("eight", String.valueOf(8));
+            s = s.replace("nine", String.valueOf(9));
         }
         answer = Integer.parseInt(s);
         return answer;
