2601278156e870cd3791f20b67e9de2812b0b8e9
Lee Sang Hoon <nicet.shlee@gmail.com>
2022-11-27 21:47:44 +0900

    Update 문자열 내 마음대로 정렬하기.java

--- 프로그래머스/lv1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.java
+++ 프로그래머스/lv1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.java
@@ -5,14 +5,14 @@ class Solution {
         String[] answer = new String[strings.length];
         
         for (int i = 0; i < strings.length; i++) {
-            answer[i] = String.valueOf(strings[i].charAt(n));
-            strings[i] = answer[i] + strings[i];
+            answer[i] = String.valueOf(strings[i].charAt(n)); // strings의 배열의 i번째 인덱스의 n 번째 단어 뽑아내기
+            strings[i] = answer[i] + strings[i]; // n번째 단어 + stirngs배열의 i번째 인덱스 단어
         }
-        Arrays.sort(strings);
+        Arrays.sort(strings); // 맨앞글자 기준으로 정렬
         
         for (int i = 0; i < strings.length; i++) {
-            strings[i] = strings[i].substring(1,strings[i].length());
-            answer[i] = strings[i];
+            strings[i] = strings[i].substring(1,strings[i].length()); //정렬한 이후 맨 앞글자 잘라줌
+            answer[i] = strings[i]; // 정답배열에 정상적인 단어 다시 
         }
         
         return answer;
