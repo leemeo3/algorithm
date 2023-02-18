059810ac9275f16744ab905c4c03fd3413a1977d
Lee Sang Hoon <nicet.shlee@gmail.com>
2023-02-19 02:20:54 +0900

    [unrated] Title: 옹알이 (2), Time: 0.06 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/133499. 옹알이 （2）/README.md
+++ 프로그래머스/unrated/133499. 옹알이 （2）/README.md
@@ -1,10 +1,10 @@
 # [unrated] 옹알이 (2) - 133499 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/133499#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/133499) 
 
 ### 성능 요약
 
-메모리: 10.1 MB, 시간: 0.07 ms
+메모리: 10.2 MB, 시간: 0.06 ms
 
 ### 구분
 
--- 프로그래머스/unrated/133499. 옹알이 （2）/옹알이 （2）.py
+++ 프로그래머스/unrated/133499. 옹알이 （2）/옹알이 （2）.py
@@ -1,10 +1,10 @@
 def solution(babbling):
     answer = 0
-    baby = ["aya", "ye", "woo", "ma"]
+    baby = ["aya", "ye", "woo", "ma"]           # 조카가 발음할 수 있는 단어
     for i in range(len(babbling)):
-        for j in baby:
-            if j * 2 not in babbling[i] :
-                babbling[i] = babbling[i].replace(j, " ").strip()
-        if babbling[i] == '':
+        for j in baby:                          # 조카가 발음할 수 있는 단어 j
+            if j * 2 not in babbling[i] :       # 연속해서 같은 발음 나올 경우 예외처리
+                babbling[i] = babbling[i].replace(j, " ").strip()# 같은 발음 나오면 공백 치환
+        if babbling[i] == '':                   # 공백치환후 공백제거 했으므로 ''일시 카운트 1
             answer += 1
     return answer
