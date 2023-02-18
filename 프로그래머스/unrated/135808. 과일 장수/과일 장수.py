b0d009b0f09494d9d3831c769f1bdb3509211051
Lee Sang Hoon <nicet.shlee@gmail.com>
2023-02-19 02:21:15 +0900

    [unrated] Title: 과일 장수, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/135808. 과일 장수/과일 장수.py
+++ 프로그래머스/unrated/135808. 과일 장수/과일 장수.py
@@ -1,12 +1,11 @@
 def solution(k, m, score):
     answer = 0
-    cnt = 0
-    score.sort()
-    for i in range(len(score)):
-        if len(score) >= m :
-            cnt += 1
-            answer += min(score[-m:]) * m * 1
-            for j in range(m):
+    score.sort()                        # 사과 상자 정렬
+    for i in range(len(score)):         # 사과 상자만큼 반복
+        if len(score) >= m :            # 사과 상자 길이가 상자에 담을 
+                                        #사과 갯수보다 크거나 같을때까지
+            answer += min(score[-m:]) * m * 1 # 이익값 추가
+            for j in range(m):          #  상자에 넣은 사과 빼기
                 score.pop()
         else:
             break
