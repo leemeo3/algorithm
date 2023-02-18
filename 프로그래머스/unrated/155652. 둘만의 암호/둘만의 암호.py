c912e0c1e45f63ce12f426f2178efaba2df2c182
Lee Sang Hoon <nicet.shlee@gmail.com>
2023-02-19 02:20:22 +0900

    [unrated] Title: 둘만의 암호, Time: 0.25 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/unrated/155652. 둘만의 암호/둘만의 암호.py
+++ 프로그래머스/unrated/155652. 둘만의 암호/둘만의 암호.py
@@ -1,25 +1,24 @@
 def solution(s, skip, index):
     answer = ''
-    sAscii = []
-    skipAscii = []
+    sAscii = []     # s 리스트
+    skipAscii = []  # skip 리스트 
     
     for i in skip :
-        skipAscii.append(ord(i))
+        skipAscii.append(ord(i))    # skip 아스키코드로 변환
         
     for i in s :
-        cnt = 0
-        check = ord(i)
-        sAscii.clear()
-        while len(sAscii) < index :
-            check += 1
+        check = ord(i)              # check에 s를 하나씩 변환해 넣기
+        sAscii.clear()              # s 리스트 초기화
+        while len(sAscii) < index : # s 리스트 길이가 인덱스를 넘으면 종료
+            check += 1              # 한번 반복할때마다 +1 추가
             
-            if check > 122 :
+            if check > 122 :        # Z를 넘어갈 경우 a로 바꾸기
                 check = ord('a')
             
-            if check in skipAscii :
+            if check in skipAscii : # check가 스킵리스트에 있을경우 넘어감
                 continue
             else :
-                sAscii.append(check)
-        answer += chr(sAscii[index - 1])
+                sAscii.append(check) # 없다면 리스트에 추가
+        answer += chr(sAscii[index - 1]) # 리스트 제일 마지막 글자 answer
     
     return answer
