2f3b8f0beef30e7dff79e2393fee75d34c63e080
Lee Sang Hoon <nicet.shlee@gmail.com>
2023-02-19 02:21:58 +0900

    [level 3] Title: 정수 삼각형, Time: 41.07 ms, Memory: 14.4 MB -BaekjoonHub

--- 프로그래머스/lv3/43105. 정수 삼각형/정수 삼각형.py
+++ 프로그래머스/lv3/43105. 정수 삼각형/정수 삼각형.py
@@ -1,6 +1,8 @@
 def solution(triangle):
     answer = 0
+    cnt = 0
     for i in range(1, len(triangle)):                                       # i는 첫번째 배열 인덱스
+        cnt += 1
         for j in range(i + 1):                                              # j는 두번째 배열 인덱스
             if j == 0:                                                      # 제일 왼쪽일 경우
                 triangle[i][j] += triangle[i-1][j]                          # 이전인덱스의 j의 0번째값 더함
@@ -8,5 +10,5 @@ def solution(triangle):
                 triangle[i][j] += triangle[i-1][j-1]                        # 이전인덱스의 j의 끝값 더함
             else:                                                           # 가운데일 경우
                 triangle[i][j] += max(triangle[i-1][j], triangle[i-1][j-1]) # 이전인덱스의 양옆의 값중 큰값을 더함
-    answer = max(triangle[-1])                                               # 마지막 인덱스의 가장 큰값을 answer에 입력
+    answer = max(triangle[cnt])                                             # 마지막 인덱스의 가장 큰값을 answer에 입력
     return answer
