61801984fc966eaf7c1b1ed17509e9abb8220a1e
Lee Sang Hoon <nicet.shlee@gmail.com>
2023-03-01 18:25:47 +0900

    [level 1] Title: 삼총사, Time: 0.09 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/131705. 삼총사/삼총사.py
+++ 프로그래머스/lv1/131705. 삼총사/삼총사.py
@@ -3,7 +3,6 @@ def solution(number):
     number.sort()
     numLen = int(len(number)/2)
     
-    # for i in range(numLen + 1) :        # 전체 범위는 리스트의 절반까지만
     for i in range(len(number)) :
         s2 = i + 1                      # 두번째 인덱스 시작 값
         s3 = i + 2                      # 세번째 인덱스 시작 값
