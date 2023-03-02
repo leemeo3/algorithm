e2a049f75faab0cef7d762f5e982a5a7d4c97436
Lee Sang Hoon <nicet.shlee@gmail.com>
2023-03-02 13:42:13 +0900

    [level 2] Title: N개의 최소공배수, Time: 512.79 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/12953. N개의 최소공배수/N개의 최소공배수.py
+++ 프로그래머스/lv2/12953. N개의 최소공배수/N개의 최소공배수.py
@@ -1,16 +1,14 @@
 def solution(arr):
     answer = max(arr)
+    cnt = 0                             # arr내 공통되는 수 갯수
     
-    while True :
-        cnt = 0                     # arr내 공통되는 수 갯수
+    while cnt != len(arr) :   
         for i in arr :
             if answer % i == 0 :
-                cnt += 1            # 공통되는 수 갯수 +1
+                cnt += 1                # 공통되는 수 갯수 +1
             else :
+                cnt = 0
+                answer += 1             # 아니라면 최소공배수 1씩 증가
                 break
-        
-        if cnt == len(arr) :        # 공통되는 수가 arr길이와 같다면 while종료
-            break
-        else :
-            answer += 1             # 아니라면 최소공배수 1씩 증가
+                
     return answer
