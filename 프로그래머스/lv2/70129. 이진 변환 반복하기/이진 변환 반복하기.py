68e9c08867b665a054ad1abf4a6c85f838c9eee5
Lee Sang Hoon <nicet.shlee@gmail.com>
2023-03-12 20:41:52 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 6.99 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/70129. 이진 변환 반복하기/이진 변환 반복하기.py
@@ -0,0 +1,18 @@
+def solution(s):
+    answer = []
+    cnt = 0             # 반복 횟수
+    zeroCnt = 0         # 제거할 0의 갯수
+    while True :
+        cnt += 1        # 반복 횟수 증가
+        check = ""      # 이진 변환 이전 check 할 수 저장
+        for i in s :    
+            if i == "1" :   #1일 경우 이진 변환 예정이므로 check 저장
+                check += i
+            else :          #1이 아닐 경우 0이기에 제거할 0 갯수 +1
+                zeroCnt += 1
+        s = format(len(check), 'b') # check의 길이 2진수 변환
+        if s == "1" :               # 이진수 변환 값이 1이면 종료
+            break
+    answer.append(cnt)
+    answer.append(zeroCnt)
+    return answer
