61ee04fd796820c0126910e546a1fab4697f2a48
Lee Sang Hoon <nicet.shlee@gmail.com>
2023-03-27 13:13:55 +0900

    [unrated] Title: 덧칠하기, Time: 10.13 ms, Memory: 11.9 MB -BaekjoonHub

--- 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.py
+++ 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.py
@@ -4,15 +4,15 @@ def solution(n, m, section):
     cnt = 0
     
     while True :
-        if len(deque) == 0 :
-            answer += 1
-            deque.append(section[cnt])
-            cnt += 1
-        elif deque[0] + m-1 >= section[cnt] :
-            cnt += 1
+        if len(deque) == 0 :                    # deque 리스트가 비어 있을때
+            answer += 1                         # 페인트칠하는 카운트는 +1
+            deque.append(section[cnt])          
+            cnt += 1                            # section 인덱스 +1
+        elif deque[0] + m-1 >= section[cnt] :   # 체크할 section 인덱스가 deque리스트 + 롤러 길이의 범위에 들었을때
+            cnt += 1                            # 그 다음 section 인덱스가 범위에 드는지 확인하기 위해 +1
         else :
             deque.pop(0)
         
-        if len(section) == cnt :
+        if len(section) == cnt :                # 체크할 section의 인덱스 끝까지 도달했을때
             break
     return answer
