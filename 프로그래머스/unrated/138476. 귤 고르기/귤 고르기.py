9b121bd9c896ac925446b2a58895d74833c31342
Lee Sang Hoon <nicet.shlee@gmail.com>
2023-03-28 12:21:31 +0900

    [unrated] Title: 귤 고르기, Time: 29.26 ms, Memory: 21.3 MB -BaekjoonHub

--- 프로그래머스/unrated/138476. 귤 고르기/귤 고르기.py
+++ 프로그래머스/unrated/138476. 귤 고르기/귤 고르기.py
@@ -1,13 +1,13 @@
 from collections import Counter
 
 def solution(k, tangerine):
-    answer = 0
-    count = Counter(tangerine).most_common()
+    answer = 0              
+    count = Counter(tangerine).most_common()    # 중복이 많은순으로 정렬
     
-    for i in range(len(count)) :
-        k -= count[i][1]
-        answer += 1
+    for i in count :
+        k -= i[1]                               # 중복값의 value
+        answer += 1                             # 최솟값 cnt 추가
         
-        if k == 0 or k < 0 :
+        if k <= 0 :                             # k가 0이거나 0보다 작을때
             break
     return answer
